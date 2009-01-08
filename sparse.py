64cce2f22e347fdcdff7dd02a794c9c9833e6f4d
Kragen Javier Sitaker <kragen@pobox.com>
2009-01-08 17:56:59 -0300

    refactored sparse.py so maybe I can call it from somewhere else

--- sparse.py
+++ sparse.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 import sys
 
-def output_line(line, offset):
+def output_line(line, offset, output):
     if line.endswith('\n'): line = line[:-1]
-    print line, offset
+    output.write('%s %s\n' % (line, offset))
 
-chunksize = int(sys.argv[1])
-infile = file(sys.argv[2])
-offset = 0
+def main(chunksize_str, infilename, output):
+    chunksize = int(chunksize_str)
+    infile = file(infilename)
+    offset = 0
 
-output_line(infile.readline(), offset)
-while True:
-    offset += chunksize
-    infile.seek(offset)
-    junk = infile.readline()
-    if junk == '': break
-    offset += len(junk)
-    output_line(infile.readline(), offset)
+    output_line(infile.readline(), offset, output)
+    while True:
+        offset += chunksize
+        infile.seek(offset)
+        junk = infile.readline()
+        if junk == '': break
+        offset += len(junk)
+        output_line(infile.readline(), offset, output)
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.stdout)
